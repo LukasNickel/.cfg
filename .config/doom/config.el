950018b5da412cb666cdfb58b037de2893d46958
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2022-01-23 20:40:34 +0100

    Giving emacs a try
    - Basic doom config with tons of org mode goodies
    - Probably some duplicate things and some things that just dont work
    - Main inspiration comes from
      https://github.com/sunnyhasija/Academic-Doom-Emacs-Config Removed some
      things that I wont use for now and adapted stuff to make it working.
    - The idea is to put everything org related into ~/org (symlinked to the
      nextcloud) including all papers that I read from now on. Then I can
      use org roam to build a knowledge base with notes on papers and so on.
    
      Lets see how it goes

---
+++ .config/doom/config.el
@@ -0,0 +1,322 @@
+;; -*- no-byte-compile: t; -*-
+;;; I am stealing a lot from https://github.com/sunnyhasija/Academic-Doom-Emacs-Config
+;;; to get a nice org-mode workflow
+;;;
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Lukas Nickel"
+      user-mail-address "lukasnickel@outlook.de")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Fira Code" :size 15)
+      doom-variable-pitch-font (font-spec :family "Roboto" :size 15)
+      doom-big-font (font-spec :family "Fira Code" :size 24))
+
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-gruvbox)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+;; This is the default obviously, jsut to have it explicit
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+;;
+
+
+(setq org-directory "~/org"
+      org-roam-directory "~/org"
+      org-agenda-files '("~/org"))
+;;Loads of org-mode related
+;;Easier use of latex
+(use-package! org-fragtog
+  :after org
+  :hook (org-mode . org-fragtog-mode)
+  )
+
+(use-package! org-appear
+  :after org
+  :hook (org-mode . org-appear-mode)
+  :config (setq
+           org-appear-autolinks t
+           org-appear-autoentities t
+           org-appear-autosubmarkers t ))
+
+;; I hope this works. Having a Zettelkasten sounds great
+(setq org-roam-v2-ack t)
+
+(use-package! org-roam
+  :after org
+  :config
+  (setq org-roam-v2-ack t)
+  (setq org-roam-mode-sections
+        (list #'org-roam-backlinks-insert-section
+              #'org-roam-reflinks-insert-section
+              #'org-roam-unlinked-references-insert-section))
+  (org-roam-setup))
+
+(setq org-cite-global-bibliography "~/org/references/library.bib")
+
+;; That sounds hot
+(use-package! org-roam-ui
+  :after org-roam
+  :config
+  (setq org-roam-ui-open-on-start nil)
+  (setq org-roam-ui-browser-function #'xwidget-webkit-browse-url))
+
+;; I guess thats just what a new file will list basically?
+;; Ah its for new org roam nodes nice
+;; person is useless to me and doenst org-ref use the things below anyway?
+(after! org-roam
+    (setq org-roam-capture-templates
+          `(("s" "standard" plain "%?"
+     :if-new
+     (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+      "#+title: ${title}\n#+filetags: \n\n ")
+     :unnarrowed t)
+        ("d" "definition" plain
+         "%?"
+         :if-new
+         (file+head "${slug}.org" "#+title: ${title}\n#+filetags: definition \n\n* Definition\n\n\n* Examples\n")
+         :unnarrowed t)
+        ("r" "ref" plain "%?"
+           :if-new
+           (file+head "references/notes/${citekey}.org"
+           "#+title: ${title}\n
+\n#+filetags: reference ${keywords} \n
+\n* ${title}\n\n
+\n* Summary
+\n\n\n* Rough note space\n")
+           :unnarrowed t)
+          ("p" "person" plain "%?"
+           :if-new
+           (file+head "${slug}.org" "%^{relation|some guy|family|friend|colleague}p %^{birthday}p %^{address}p
+#+title:${slug}\n#+filetags: :person: \n"
+                      :unnarrowed t)))))
+
+;; Hype
+(use-package! org-ref
+    ;:after org-roam
+    :config
+    (setq
+         org-ref-completion-library 'org-ref-ivy-cite
+         org-ref-get-pdf-filename-function 'org-ref-get-pdf-filename-helm-bibtex
+         bibtex-completion-bibliography (list "~/org/references/library.bib")
+         bibtex-completion-notes "~/org/references/notes/bibnotes.org"
+         org-ref-note-title-format "* %y - %t\n :PROPERTIES:\n  :Custom_ID: %k\n  :NOTER_DOCUMENT: %F\n :ROAM_KEY: cite:%k\n  :AUTHOR: %9a\n  :JOURNAL: %j\n  :YEAR: %y\n  :VOLUME: %v\n  :PAGES: %p\n  :DOI: %D\n  :URL: %U\n :END:\n\n"
+         org-ref-notes-directory "~/org/references/notes/"
+         org-ref-notes-function 'orb-edit-notes
+         org-ref-pdf-directory "~/org/references"
+    ))
+(setq ivy-bibtex-default-action 'ivy-bibtex-insert-citation)
+
+(after! org-ref
+(setq
+ bibtex-completion-library-path "~/org/references"
+ bibtex-completion-notes-path "~/org/references/notes/"
+ bibtex-completion-bibliography "~/org/references/library.bib"
+ bibtex-completion-pdf-field "file"
+ bibtex-completion-notes-template-multiple-files
+ (concat
+  "#+TITLE: ${title}\n"
+  "#+ROAM_KEY: cite:${=key=}\n"
+  "* TODO Notes\n"
+  ":PROPERTIES:\n"
+  ":Custom_ID: ${=key=}\n"
+  ":NOTER_DOCUMENT: %(orb-process-file-field \"${=key=}\")\n"
+  ":AUTHOR: ${author-abbrev}\n"
+  ":JOURNAL: ${journaltitle}\n"
+  ":DATE: ${date}\n"
+  ":YEAR: ${year}\n"
+  ":DOI: ${doi}\n"
+  ":URL: ${url}\n"
+  ":END:\n\n"
+  )
+ )
+)
+
+;; Why not
+(use-package! org-roam-bibtex
+  :after org-roam
+  :hook (org-mode . org-roam-bibtex-mode)
+  :config
+  (require 'org-ref)
+  (setq orb-preformat-keywords
+   '("citekey" "title" "url" "file" "author-or-editor" "keywords" "pdf" "doi" "author" "tags" "year" "author-bbrev")))
+;)
+
+;; Thats a lot of settings
+        (after! org
+(setq org-ellipsis " ▾ ")
+  (appendq! +ligatures-extra-symbols
+          `(:checkbox      "☐"
+            :pending       "◼"
+            :checkedbox    "☑"
+            :list_property "∷"
+            :em_dash       "—"
+            :ellipses      "…"
+            :arrow_right   "→"
+            :arrow_left    "←"
+            :title         nil
+            :subtitle      "𝙩"
+            :author        "𝘼"
+            :date          "𝘿"
+            :property      ""
+            :options       "⌥"
+            :startup       "⏻"
+            :macro         "𝓜"
+            :html_head     "🅷"
+            :html          "🅗"
+            :latex_class   "🄻"
+            :latex_header  "🅻"
+            :beamer_header "🅑"
+            :latex         "🅛"
+            :attr_latex    "🄛"
+            :attr_html     "🄗"
+            :attr_org      "⒪"
+            :begin_quote   "❝"
+            :end_quote     "❞"
+            :caption       "☰"
+            :header        "›"
+            :results       "🠶"
+            :begin_export  "⏩"
+            :end_export    "⏪"
+            :properties    ""
+            :end           "∎"
+            :priority_a   ,(propertize "⚑" 'face 'all-the-icons-red)
+            :priority_b   ,(propertize "⬆" 'face 'all-the-icons-orange)
+            :priority_c   ,(propertize "■" 'face 'all-the-icons-yellow)
+            :priority_d   ,(propertize "⬇" 'face 'all-the-icons-green)
+            :priority_e   ,(propertize "❓" 'face 'all-the-icons-blue)
+            :roam_tags nil
+            :filetags nil))
+(set-ligatures! 'org-mode
+  :merge t
+  :checkbox      "[ ]"
+  :pending       "[-]"
+  :checkedbox    "[X]"
+  :list_property "::"
+  :em_dash       "---"
+  :ellipsis      "..."
+  :arrow_right   "->"
+  :arrow_left    "<-"
+  :title         "#+title:"
+  :subtitle      "#+subtitle:"
+  :author        "#+author:"
+  :date          "#+date:"
+  :property      "#+property:"
+  :options       "#+options:"
+  :startup       "#+startup:"
+  :macro         "#+macro:"
+  :html_head     "#+html_head:"
+  :html          "#+html:"
+  :latex_class   "#+latex_class:"
+  :latex_header  "#+latex_header:"
+  :beamer_header "#+beamer_header:"
+  :latex         "#+latex:"
+  :attr_latex    "#+attr_latex:"
+  :attr_html     "#+attr_html:"
+  :attr_org      "#+attr_org:"
+  :begin_quote   "#+begin_quote"
+  :end_quote     "#+end_quote"
+  :caption       "#+caption:"
+  :header        "#+header:"
+  :begin_export  "#+begin_export"
+  :end_export    "#+end_export"
+  :results       "#+RESULTS:"
+  :property      ":PROPERTIES:"
+  :end           ":END:"
+  :priority_a    "[#A]"
+  :priority_b    "[#B]"
+  :priority_c    "[#C]"
+  :priority_d    "[#D]"
+  :priority_e    "[#E]"
+  :roam_tags     "#+roam_tags:"
+  :filetags      "#+filetags:")
+(plist-put +ligatures-extra-symbols :name "⁍")
+)
+
+(with-eval-after-load 'org
+  (plist-put org-format-latex-options :background 'default))
+
+;; Why though?
+(define-minor-mode prot/variable-pitch-mode
+  "Toggle 'mixed-pitch-modei, except for programming modes"
+  :init-value nil
+  :global nil
+  (if prot/variable-pitch-mode
+      (unless (derived-mode-p 'prog-mode)
+        (variable-pitch-mode 1))
+    (variable-pitch-mode -1)))
+
+
+;; Thats a good idea, lets see if I can get used to it
+;; I can remove some though, no?
+(map! :leader
+      (:prefix-map ("r" . "regular")
+       :desc "find file"            "f"   #'org-roam-node-find
+       :desc "find ref"             "F"   #'org-roam-ref-find
+       :desc "center scroll"        "s"   #'prot/scroll-center-cursor-mode
+       :desc "start taking notes"   "S"   #'org-noter
+       :desc "toggle buffer"        "b"   #'org-roam-buffer-toggle
+       :desc "insert note"          "i"   #'org-roam-node-insert
+       :desc "server"               "g"   #'org-roam-server
+       :desc "quit notes"           "q"   #'org-noter-kill-session
+       :desc "tag (roam)"           "t"   #'org-roam-tag-add
+       :desc "tag (org)"            "T"   #'org-set-tags-command
+       :desc "change nano-theme"    "n"   #'nano-toggle-theme
+       :desc "rebuid db"            "d"   #'org-roam-db-build-cache
+       :desc "cite"                 "c"   #'org-ref-insert-cite-link
+       :desc "thesaurus this word"  "w"  #'powerthesaurus-lookup-word-at-point
+       :desc "thesaurus lookup word" "W"   #'powerthesaurus-lookup-word
+       :desc "outline"              "o"   #'org-ol-tree
+       (:prefix  ("r" . "orui")
+                :desc "orui-mode" "r" #'org-roam-ui-mode
+                :desc "zoom" "z" #'orui-node-zoom
+                :desc "open" "o" #'orui-open
+                :desc "local" "l" #'orui-node-local
+                :desc "sync theme" "t" #'orui-sync-theme
+                :desc "follow" "f" #'orui-follow-mode)
+     )
+)
+
+(global-set-key [f1] (lambda () (interactive) (message "%s" org-agenda-files)))
