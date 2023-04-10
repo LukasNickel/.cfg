bafb6ca30b70f14562caff449d7b5b631be1b483
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2023-04-10 13:14:01 +0200

    Diss template

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -269,7 +269,20 @@
              ("\\subsection{%s}" . "\\subsection*{%s}")
              ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
              ("\\paragraph{%s}" . "\\paragraph*{%s}")
-             ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+             ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))
+          )
+(add-to-list 'org-latex-classes
+          '("diss"
+             "\\input{~/Nextcloud/diss/header.tex}
+             [NO-DEFAULT-PACKAGES]
+             [NO-PACKAGES]"
+             ("\\section{%s}" . "\\section*{%s}")
+             ("\\subsection{%s}" . "\\subsection*{%s}")
+             ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+             ("\\paragraph{%s}" . "\\paragraph*{%s}")
+             ("\\subparagraph{%s}" . "\\subparagraph*{%s}")
+             )
+          )
 
 (setq fancy-splash-image "~/.config/doom/blackhole.png")
 ;; true transparency
